fa86ad3e547ea61d09f390583d8dddd4a8be7886
BudiGabe <razvan.gabriel.budaca@gmail.com>
2023-03-29 12:25:18 +0300

    Backend done

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -5,11 +5,15 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // deploy contracts here:
+  // Contract deployed here
+  const NFTMarketplaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
+  const nftMarketplace = await NFTMarketplaceFactory.deploy();
   
-  
-  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  console.log("Smart contract address: ", nftMarketplace.address);
+
+  // For each contract, pass the deployed contract and name to this function
+  // to save a copy of the contract ABI and address to the front end.
+  saveFrontendFiles(nftMarketplace, "MusicNFTMarketplace");
 }
 
 function saveFrontendFiles(contract, name) {
